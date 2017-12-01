277add1264b649a7f726354ef0547f01bacba860
Vivien <vdebona@umanit.fr>
2017-12-01 22:10:49 +0100

    Jour 1 - Partie 2

--- 01.php
+++ 01.php
@@ -4,19 +4,34 @@
     $arr = str_split($input);
     $sum = 0;
     $length = count($arr);
+    $mid = floor($length / 2);
+    $sum2 = 0;
 
     for ($i = 0; $i < $length; $i++) {
-        $number = $arr[$i];
+        $number = (int) $arr[$i];
 
+        // Partie 1
         if ($i === $length - 1) {
-            $next = reset($arr);
+            $next = (int) reset($arr);
         } else {
-            $next = $arr[$i + 1];
+            $next = (int) $arr[$i + 1];
         }
 
         if ($number === $next) {
             $sum += $number;
         }
+
+        // Partie 2
+        if ($i < $mid) {
+            $other = (int) $arr[$mid + $i];
+        } else {
+            $other = (int) $arr[$i - $mid];
+        }
+
+        if ($number === $other) {
+            $sum2 += $number;
+        }
     }
 
-    echo 'Result: '.$sum;
+    echo 'Result: '.$sum.'<br>';
+    echo 'Result 2: '.$sum2;
