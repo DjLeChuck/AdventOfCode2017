d5e95d90c18cff1619a1b65d8652b049a891f3bb
Vivien <vdebona@umanit.fr>
2017-12-07 14:03:00 +0100

    Jour 7 - Partie 2 suite (mais fausse)

--- 07.php
+++ 07.php
@@ -70,16 +70,23 @@
 
     $first = reset($weights);
     $second = next($weights);
+    $last = end($weights);
     $wrong = null;
+    $goodWeight = null;
+    $badWeight = null;
 
     if ($first !== $second) {
         $wrong = array_search($first, $weights);
-    } else {
-        $last = end($weights);
-
-        if ($first !== $last) {
-            $wrong = array_search(end($weights), $weights);
-        }
+        $goodWeight = $second;
+        $badWeight = $first;
+    } else if ($first !== $last) {
+        $wrong = array_search(end($weights), $weights);
+        $goodWeight = $first;
+        $badWeight = $last;
     }
+var_dump($weights);
+    if (null !== $wrong) {
+        $diff = abs($badWeight - $goodWeight);
 
-    var_dump($wrong);
+        echo 'Result2: '.(abs($names[$wrong]['weight'] - $diff))."\n";
+    }
